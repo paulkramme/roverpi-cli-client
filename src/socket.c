44e65e0994abfb306bafaa6e5d537a00e0586ecc
Paul Kramme <pjkramme@gmail.com>
2016-08-10 00:02:19 +0200

    Add socket client

--- src/socket.c
+++ src/socket.c
@@ -8,5 +8,24 @@ int test(void)
 
 int socket(void)
 {
-	return 0;
+	int client_socket;
+	char sendline[256];
+	int input = 0;
+	struct sockaddr_in servaddr;
+	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	//bzero(&servaddr,sizeof servaddr);
+	memset(&servaddr, '\0', sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(22000);
+	inet_pton(AF_INET, "192.168.247.15", &(servaddr.sin_addr));
+	connect(client_socket, (struct sockaddr *) &servaddr, sizeof(servaddr));
+	while(1)
+	{
+		bzero( sendline, 256);
+		//fgets(sendline,2,stdin); /*stdin = 0 , for standard input */
+		input = getch();
+		sendline[0] = input;
+		write(client_socket, sendline, strlen(sendline)+1);
+	}
+	/*return 0;*/
 }
